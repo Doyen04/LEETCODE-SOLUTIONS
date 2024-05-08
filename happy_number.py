de30877709ed29fe0d3101f1dff6fd20ddf5e3f1
Doyen04 <solaopeyemi93@gmail.com>
2024-05-08 21:42:16 +0100

    ADDED CONDITION TO PREVENT INFINITE LOOP

--- happy_number.py
+++ happy_number.py
@@ -1,10 +1,10 @@
-num = 456789
+num = 7
 
 def happy_number(num):
 
     h_num = num
 
-    while h_num != 4:
+    while h_num != 4 and h_num != 1:
 
         split = list((lambda x : x)(str(h_num)))
         h_num = sum(list(map(lambda x: int(x)**2, split)))
