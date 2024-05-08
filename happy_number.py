8eb791262b43d5905ac46c719d9e9ff312c1b98c
Doyen04 <solaopeyemi93@gmail.com>
2024-05-08 21:23:04 +0100

    ADDED A CHECK FOR UNHAPPINESS

--- happy_number.py
+++ happy_number.py
@@ -2,14 +2,13 @@ num = 4567
 
 def happy_number(num):
 
-    if len(str(num)) <= 1: return False
     h_num = num
 
-    while len(str(h_num)) > 1:
+    while h_num != 4:
 
         split = list((lambda x : x)(str(h_num)))
         h_num = sum(list(map(lambda x: int(x)**2, split)))
-        print(h_num)
+        print(h_num) 
     return True if h_num == 1 else False
     
 print(happy_number(num))
